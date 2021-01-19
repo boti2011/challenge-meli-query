b10219dc6ad7709efe4465151e8f6e6a10df2c25
boti2011 <bcamilo@gmail.com>
2021-01-18 22:14:28 -0500

    :sparkles: HU-6: Add swagger documentation

--- src/main/java/co/com/meli/challenge/search/alerts/AlertsApplication.java
+++ src/main/java/co/com/meli/challenge/search/alerts/AlertsApplication.java
@@ -2,7 +2,9 @@ package co.com.meli.challenge.search.alerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class AlertsApplication {
 
