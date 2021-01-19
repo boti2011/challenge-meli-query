befdf2e2c33bff616042932299f290cade372cc4
boti2011 <bcamilo@gmail.com>
2021-01-18 22:26:16 -0500

    :sparkles: HU-6: Add documentation to response object

--- src/main/java/co/com/meli/challenge/search/alerts/model/SearchAlertResponse.java
+++ src/main/java/co/com/meli/challenge/search/alerts/model/SearchAlertResponse.java
@@ -1,5 +1,6 @@
 package co.com.meli.challenge.search.alerts.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.util.Map;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SearchAlertResponse {
 
+  @ApiModelProperty(notes = "Status response")
   private Integer status;
+
+  @ApiModelProperty(notes = "Description of response")
   private String description;
+
+  @ApiModelProperty(notes = "Data of alerts and pagination information to show in front")
   private Map<String, Object> data;
 }
